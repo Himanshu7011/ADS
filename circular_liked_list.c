52a945434c6ff831b6b75541a7ada8614558a820
Mercury <hgupta107@gmail.com>
2022-10-30 22:18:01 -0700

    Circular Linked List Full Code. Added Changes For Traverse And Delete Module.

--- circular_liked_list.c
+++ circular_liked_list.c
@@ -28,6 +28,7 @@ void insertStart(struct node **head, struct node **rear, int num)
 	}
 }
 
+void traverseNode(struct node *);
 void deleteNode(struct node **ptr, struct node **rear)
 {
 
@@ -36,39 +37,67 @@ void deleteNode(struct node **ptr, struct node **rear)
 	if(*ptr == NULL)
 		return;
 	if((*ptr)->next == *ptr) { /* To handle the case, when only last node is there in linked list to delete. */
+		printf("\nElexment %d deleted.\n", (*ptr)->data);
 		free(*ptr);
-		return;
+		*ptr = NULL;    /* Set the pointer to NULL once freed, else this will lead to dangling pointer */
+		goto traverse;
 	}
 	*ptr = (*ptr)->next;
 	(*rear)->next = *ptr;
+	printf("\nElexment %d deleted.\n", freeNode->data);
 	free(freeNode);
 	freeNode = NULL;
+traverse:
+	traverseNode(*ptr);
 }
 
 /* Print the elements of linked list */
-void traverseNode(struct node *ptr)
+void traverseNode(struct node *head)
 {
 	/* Fix the traverse code */
+	struct node *temp = head;
+	
+	if((head == NULL))
+		return;
+
+	do {
+		printf(" %d", head->data);
+		head = head->next;
+	} while(head != temp);
+
+	printf("\n");
 }
 
 // main function.
 int main()
 {
-        int i = 0, x = 100;
+        int i = 0, x, num, loopBreaker = 1;
         struct node *head = NULL, *rear = NULL; /* Create head point to point to head of the linked list. */
 
-	insertStart(&head, &rear, 20);
-	insertStart(&head, &rear, 30);
-	insertStart(&head, &rear, 40);
-	insertStart(&head, &rear, 50);
-	insertStart(&head, &rear, 60);
-	insertStart(&head, &rear, 70);
-	traverseNode(head);
-	deleteNode(&head, &rear);
-	deleteNode(&head, &rear);
-	deleteNode(&head, &rear);
-	deleteNode(&head, &rear);
-	deleteNode(&head, &rear);
-	deleteNode(&head, &rear);
-	traverseNode(head);
+	while(loopBreaker) {
+		printf("Enter your choice:\n   1. Insert an element in Circular linked list\n   2. Delete an element in Circular linked list\n   3. Print Circular linked list\n   4. Exit\n");
+		scanf("%d", &x);
+
+		switch(x) {
+			case 1:
+				printf("Enter a num: ");
+				scanf("%d", &num);
+				insertStart(&head, &rear, num);
+				printf("\nElexment %d added.\n", num);
+				break;
+
+			case 2:
+				deleteNode(&head, &rear);
+				break;
+
+			case 3:
+				traverseNode(head);
+				break;
+			case 4:
+			default:
+				loopBreaker = 0;
+				printf("**** Invalid input ****\n");
+				break;	
+		}
+	}
 }
